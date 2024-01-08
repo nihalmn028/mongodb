5247e3094c51271b4a204898a4a45676906fbd33
nihalmn028 <148523248+nihalmn028@users.noreply.github.com>
2024-01-08 13:07:29 +0530

    third commit

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express=require('express')
 const app=express()
 app.use(express.json())
+//middleware function
+
+app.use((req,res,next)=>{
+  // console.log("request recieved")
+
+  console.log(`${req.url}`)
+  console.log(`${req.method}`)
+
+console.log(Date.now())
+  next()
+})
 
 
 const dotenv=require('dotenv').config()
